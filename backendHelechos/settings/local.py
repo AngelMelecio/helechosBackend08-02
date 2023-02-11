06b336013acfbcdcd4ebff3e4520ee8b77a249a6
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-02-11 14:50:55 -0600

    Settings for change database

--- backendHelechos/settings/local.py
+++ backendHelechos/settings/local.py
@@ -10,8 +10,15 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'cesaradmin',
+        'NAME':'helechosData',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
