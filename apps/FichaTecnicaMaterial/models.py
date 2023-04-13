197ec401024c73d49e48c459943805eca46fb5bf
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-04-13 16:53:33 -0600

    Reestructuracion de datos

--- apps/FichaTecnicaMaterial/models.py
+++ apps/FichaTecnicaMaterial/models.py
@@ -9,7 +9,7 @@ class FichaTecnicaMaterial(models.Model):
     material = models.ForeignKey(Material, on_delete=models.DO_NOTHING)
     guiaHilos = models.CharField(max_length=200,null=True, blank=True)
     hebras = models.IntegerField(null=True,blank=True)
-    peso =models.DecimalField(decimal_places=2, max_digits=5)
+    peso =models.DecimalField(decimal_places=4, max_digits=8)
 
     def __str__(self):
             return "{} {} {} {}".format(self.fichaTecnica.modelo.nombre+'-',self.fichaTecnica.nombre,self.guiaHilos+'-', self.material)
