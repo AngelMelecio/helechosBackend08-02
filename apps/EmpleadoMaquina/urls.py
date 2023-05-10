77128132ed75ed283d66841124d868a6f2cf5a88
AngelSanabriaMelecio <s19120122@alumnos.itsur.edu.mx>
2023-05-10 09:56:24 -0600

    API Empleado_Maquinas modificado

--- apps/EmpleadoMaquina/urls.py
+++ apps/EmpleadoMaquina/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from apps.EmpleadoMaquina.api import empleado_maquina_api_view,empleado_maquina_detail_api_view,empleado_maquinas_api_view
+from apps.EmpleadoMaquina.api import empleado_maquina_api_view,empleado_maquinas_detail_api_view,empleado_maquinas_api_view
 
 urlpatterns = [
     path('empleados_maquina/', empleado_maquina_api_view, name='empleados_maquina_api'),
-    path('empleados_maquina/<int:pkEmpleado>', empleado_maquina_detail_api_view, name='empleado_maquina_detail_api_view'),
+    path('empleado_maquinas/<int:pkEmpleado>', empleado_maquinas_detail_api_view, name='empleado_maquina_detail_api_view'),
     path('empleado_maquinas/', empleado_maquinas_api_view, name='empleado_maquinas_api_view')
 
 ]
