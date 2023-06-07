7ac4b51e1c8e0ca23a63b91a04d69bef93d042a7
AngelMelecio <joseangelsm50@gmail.com>
2023-06-07 14:38:37 -0600

    pedidos, detalles, produccion POST en cadena

--- apps/Pedidos/models.py
+++ apps/Pedidos/models.py
@@ -5,7 +5,7 @@ from apps.Modelos.models import Modelo
 class Pedido(models.Model):
     idPedido = models.AutoField(auto_created=True, primary_key=True)
     modelo = models.ForeignKey(
-        Modelo, on_delete=models.CASCADE, null=True, blank=True)
+        Modelo, on_delete=models.CASCADE, null=False, blank=False)
     fechaRegistro = models.DateField(null=True, blank=True)
     fechaEntrega = models.DateField(null=True, blank=True)
     def __str__(self):
