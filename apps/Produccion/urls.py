771fe759ce177fd3b9869624558fdf6b97d235ab
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-07-17 11:42:01 -0600

    Impresion de etiquetas

--- apps/Produccion/urls.py
+++ apps/Produccion/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from apps.Produccion.api import produccion_api_view,produccion_detail_api_view,get_produccion_with_registros_by_pedido
+from apps.Produccion.api import produccion_api_view,update_produccion_impresion,get_produccion_with_registros_by_pedido
 
 urlpatterns = [
     path('produccion/', produccion_api_view, name='produccion_api_view'),
-    path('produccion/<int:pk>', produccion_detail_api_view, name='produccion_detail_api_view'),
+    path('produccionPrint/', update_produccion_impresion, name='update_produccion_impresion'),
     path('produccionByPedido/<int:pk>', get_produccion_with_registros_by_pedido, name="get_produccion_with_registros_by_detallePedido")
 ]
