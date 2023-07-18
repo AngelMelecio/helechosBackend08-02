d8dc2623fc1df91fc7179206dcf903cafcb11e28
AngelMelecio <joseangelsm50@gmail.com>
2023-07-17 21:38:47 -0600

    Post Registros, PUT Produccion

--- apps/Registros/api.py
+++ apps/Registros/api.py
@@ -3,47 +3,96 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.decorators import parser_classes
 from apps.Registros.models import Registro
+from apps.Empleados.models import Empleado
 from apps.Registros.serializers import RegistroSerializer, RegistroSerializerListar
 from apps.Produccion.models import Produccion
+from apps.Empleados.serializers import EmpleadoSerializer
 from apps.Produccion.serializers import ProduccionSerializerPostRegistro
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.parsers import JSONParser
+from django.utils import timezone
 
-@api_view(['GET','POST'])
-@parser_classes([MultiPartParser , JSONParser])
+
+@api_view(['GET', 'POST'])
+@parser_classes([MultiPartParser, JSONParser])
 def registro_api_view(request):
     # list
     if request.method == 'GET':
         registros = Registro.objects.all()
-        registro_serializer = RegistroSerializerListar(registros,many=True)
-        return Response( registro_serializer.data, status=status.HTTP_200_OK )
+        registro_serializer = RegistroSerializerListar(registros, many=True)
+        return Response(registro_serializer.data, status=status.HTTP_200_OK)
 
     # Create
     elif request.method == 'POST':
-        
-        response = []
+
         registros = request.data
+        fcha = timezone.now()
+        empleado = Empleado.objects.filter(
+            idEmpleado=registros[0].get('empleado')).first()
+        empl_srlzr = EmpleadoSerializer(empleado)
+        
 
+        rgtrs = []
         for reg in registros:
+            # Obtenemos el estado actual de la etiqueta
             idPrd = reg.get('produccion')
-            prd = Produccion.objects.filter( idProduccion = idPrd ).first()
+            prd = Produccion.objects.filter(idProduccion=idPrd).first()
             prd_srlzr = ProduccionSerializerPostRegistro(prd)
+            
+            empl = reg.get('empleado')
+            maq = reg.get('maquina')
+            dpto = reg.get('departamento')
+            trno = reg.get('turno')
 
-            estacionAnterior = prd_srlzr.data.get('estacionActual')
-            estacionNueva = prd_srlzr.data['detallePedido']['rutaProduccion'][estacionAnterior]
+            estacionAnterior \
+                = prd_srlzr.data.get('estacionActual')
+            estacionNueva \
+                = prd_srlzr.data['detallePedido']['rutaProduccion'][estacionAnterior.lower()]
+            modelo \
+                = prd_srlzr.data['detallePedido']['pedido']['modelo']['nombre']
 
-            response.append( 
-                {
+            messg = ""
+            ok = False
+
+            # validar departamento:
+            if (dpto.lower() == estacionAnterior.lower()):
+                messg = estacionAnterior + " --> " + estacionNueva
+                ok = True
+            else:
+                messg = "El departamento no coincide con la estacion actual"
+
+            rgtrs.append({
+                'modelo': modelo,
                 'numEtiqueta': prd_srlzr.data.get('numEtiqueta'),
-                'Detalles': estacionAnterior + " --> " + estacionNueva
-                }
-            )#prd_srlzr.data)
-            """
-            """
+                'ok': ok,
+                'Detalles': messg
+            })
 
+            if ok:
+                #POST de registro
+                reg_serializer = RegistroSerializer(data={
+                    'empleado': empl,
+                    'maquina': maq,
+                    'produccion': idPrd,
+                    'turno': trno,
+                    'fechaCaptura': fcha,
+                    'departamento': dpto
+                })
+                if reg_serializer.is_valid():
+                    reg_serializer.save()
 
+                # PUT de produccion
+                prd.estacionActual = estacionNueva
+                prd.save()
 
-        return Response( response, status=status.HTTP_200_OK)
+        response = {
+            'empleado': empl_srlzr.data.get('nombre') + " " + empl_srlzr.data.get('apellidos'),
+            'fecha': fcha,
+            'registros': rgtrs,
+            'departamento': registros[0].get('departamento')
+        }
+
+        return Response(response, status=status.HTTP_200_OK)
         """
             if p_s.is_valid() :
             else:
@@ -56,40 +105,41 @@ def registro_api_view(request):
         return Response( registro_serializer.errors, status=status.HTTP_400_BAD_REQUEST )
         """
 
-@api_view(['GET','PUT','DELETE'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 @parser_classes([MultiPartParser, JSONParser])
-def registro_detail_api_view(request, pk=None ):
+def registro_detail_api_view(request, pk=None):
     # Queryset
-    registro = Registro.objects.filter( idRegistro = pk ).first()
-    
+    registro = Registro.objects.filter(idRegistro=pk).first()
+
     # Validacion
     if registro:
         # Retrieve
         if request.method == 'GET':
-            registro_serializer =  RegistroSerializer(registro)
-            return Response( registro_serializer.data, status=status.HTTP_200_OK )
-        
+            registro_serializer = RegistroSerializer(registro)
+            return Response(registro_serializer.data, status=status.HTTP_200_OK)
+
         # Update
         elif request.method == 'PUT':
-            registro_serializer = RegistroSerializer(registro, data = request.data)
-            print( 'PUTTING' )
+            registro_serializer = RegistroSerializer(
+                registro, data=request.data)
+            print('PUTTING')
 
             if registro_serializer.is_valid():
                 registro_serializer.save()
-                return Response( {'message':'¡Registro actualizado correctamente!'}, status=status.HTTP_200_OK)
-            return Response(registro_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-        
+                return Response({'message': '¡Registro actualizado correctamente!'}, status=status.HTTP_200_OK)
+            return Response(registro_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         # Delete
         elif request.method == 'DELETE':
-            registro = Registro.objects.filter( idRegistro = pk ).first()
+            registro = Registro.objects.filter(idRegistro=pk).first()
             registro.delete()
             return Response(
-                {'message':'¡Registro eliminado correctamente!'}, 
+                {'message': '¡Registro eliminado correctamente!'},
                 status=status.HTTP_200_OK
             )
-   
+
     return Response(
-        {'message':'No se encontró el registro'}, 
+        {'message': 'No se encontró el registro'},
         status=status.HTTP_400_BAD_REQUEST
     )
-        
