865c9a10a528479a5a24e7ec89ab04ff3955857d
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-08-14 13:45:30 -0600

    Ajustes 14/08/2023

--- apps/Pedidos/models.py
+++ apps/Pedidos/models.py
@@ -8,5 +8,6 @@ class Pedido(models.Model):
         Modelo, on_delete=models.CASCADE, null=False, blank=False)
     fechaRegistro = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     fechaEntrega = models.DateField(null=True, blank=True)
+    ordenCompra = models.CharField(max_length=50, null=True, blank=True)
     def __str__(self):
         return "{} {}".format(self.idPedido, self.modelo.nombre)
